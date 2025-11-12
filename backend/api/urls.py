8f6276c96791a1af4c01a0abe5eb72f1fae3f308
fsantos2 <ff@42lisboa.com>
2025-11-12 13:22:01 +0000

    Changed the path of the view of all the tasks

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path("tasks/", views.TasksListView.as_view()),
+    path("tasks", views.TasksListView.as_view()),
     path("tasks/<int:task_id>", views.SingleTaskListView.as_view()),
 ]
 
